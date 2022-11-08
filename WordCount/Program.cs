d5f6da6bbdb99d3b11ed64479a7917fa8a1d160d
Demyst1fy <demyst1fy@outlook.com>
2022-11-08 14:03:23 +0100

    Projektdateien hinzufügen.

---
+++ WordCount/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(args);
+        }
+    }
+}
