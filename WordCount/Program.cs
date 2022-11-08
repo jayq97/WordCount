27c98b077a4c772fc8b16361987430e95ee1826a
Garucx <90602497+Garucx@users.noreply.github.com>
2022-11-08 18:15:19 +0100

    args

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -1,12 +1,57 @@
-﻿namespace MyApp // Note: actual namespace depends on the project name.
+﻿using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var input = Console.ReadLine();
 
-            Console.WriteLine(args);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Missing Argukments");
+                return;
+            }
+
+            if (args.Length == 1)
+            {
+                if (args[0].ToLower() == "-r")
+                {
+                    Console.WriteLine("Missing Path");
+                    return;
+                }
+                if (!Directory.Exists(args[0])){
+                    Console.WriteLine("Path doesn't exist");
+                    return ;
+                }
+
+                // nicht rekusiv
+
+            }
+            if (args.Length == 2)
+            {
+                if(!args.Any(x => x.ToLower() == "-r"))
+                {
+                    Console.WriteLine("Not correct Arguments");
+                    return;
+                }
+
+                string[] path = Array.FindAll(args, x => x.ToLower() != "-r" );
+
+                if (!Directory.Exists(path[0]))
+                {
+                    Console.WriteLine("Path doesn't exist");
+                    return;
+                }
+
+                // Rekursiv
+            }
+            if (args.Length > 2)
+            {
+                Console.WriteLine("To Many Arguments");
+                return;
+            }
+            Console.ReadKey();
         }
     }
 }
